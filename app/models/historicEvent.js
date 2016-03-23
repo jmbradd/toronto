ca6fd467b3313cd8cec700ec815b4b6c2ef01ea5
James Bradd <jmbradd@gmail.com>
2016-03-22 22:56:45 -0700

    Difficulty assessment
    Quick and dirty pre hook when saving a question to re-assess the
    difficulty of the question for the next time it comes up.  Rough version
    of the question schema as well.

--- app/models/historicEvent.js
+++ app/models/historicEvent.js
@@ -8,8 +8,64 @@ var EventSchema =  new mongoose.Schema({
     year: Number,
     title: String,
     isTrue: Boolean,
-    text: String
+    text: String,
+    difficulty: Number,
+    tracking: {
+        useages: Array,
+        answers: Array
+    }
 
 });
+
+//before saving the document, reassess the difficulty
+EventSchema.pre('save', function(doc)
+{
+    //check to see if length is under 20, if so, set the length accordingly
+    var max;
+
+    if (doc.tracking.answers.length >= 20)
+    {
+        max = 20
+    }
+    else
+    {
+        max = doc.tracking.answers.length
+    }
+    var correct = 0
+    for(var i=0; i < max; i++ )
+    {
+        var ans = doc.tracking.answers[answer]
+        if (ans.correct = true)
+        {
+            correct++
+        }
+        //this is really ugly.
+        if(i == max)
+        {
+            var pct = (correct / max) * 100
+
+            if ( pct > 0 &&  pct <= 20)
+            {
+                doc.difficulty = 5
+            }
+            else if ( pct > 20 && pct <= 40)
+            {
+                doc.difficulty = 4
+            }
+            else if ( pct > 40 && pct <= 60)
+            {
+                doc.difficulty = 3
+            }
+            else if ( pct > 60 && pct <= 80)
+            {
+                doc.difficulty = 2
+            }
+            else if ( pct > 80 )
+            {
+                doc.difficulty = 1
+            }
+        }
+    }
+})
 
 module.exports = mongoose.model('historicEvent', EventSchema, "events");
